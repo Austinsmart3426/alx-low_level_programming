1c5e27b62edbcc4e856099495073fa1df4eaf55b
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-15 18:37:11 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,32 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 
-int _putchar(char);
 void print_alphabet(void);
+
 void print_alphabet_x10(void);
+
+void fibonacci(void);
+
+int _putchar(char c);
+
 int _islower(int c);
+
 int _isalpha(int c);
+
 int print_sign(int n);
+
 int _abs(int);
-int print_last_digit(int);
+
 void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
+
+int print_last_digit(int);
+
 void print_times_table(int n);
-void num(int r, int n);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
 
 #endif
