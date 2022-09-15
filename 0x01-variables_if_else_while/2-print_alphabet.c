7ee88facc41d4b65fa44a28ba0a295cbd068621e
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-15 00:16:15 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+nclude <stdio.h>
+
+/**
+ *  * main - Write a program that prints the alphabet in lowercase, followed by a new line
+ *   * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
