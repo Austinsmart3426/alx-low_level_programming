e5fcfd03f8c5908c616a7e587a11a25072c15c40
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-15 17:59:34 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-include "_putchar.h"
+#include "_putchar.h"
 /**
- * main - print putchar
- * Return: Always 0
+ *  * main - Entry point
+ *   * Description: prints 'Holberton \n'
+ *    * Return: Always 0 (success)
  */
 	int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
 	return (0);
 }
