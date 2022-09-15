cf7be286a6922dad9827b9afcd0edeb579c01fde
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-15 00:40:54 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all single digit
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	printf("%d", num);
+	putchar('\n');
+	return (0);
+}
