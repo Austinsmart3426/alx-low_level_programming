6984314b6b6ad7a57ccee415ff980ccb64faffa0
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-15 00:48:18 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * main - Write a program that prints the lowercase alphabet in reverse,
+ *   * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
