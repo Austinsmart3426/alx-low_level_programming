b3b54a37b0c779fe40533a8669c60357b19e4827
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-21 12:48:14 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Main - Entry poin
+ *
+ *Description: A function that updates a number it points to to 98
+ *
+ * @n: pointer parameter
+ * Return: returns 0 on success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * Main.h fie for 0x05. C - Pointers, arrays and strings
+ * Desc - contains all functions used
+ * Return: Always returns 0
+ */
+
+int _putchar(char c);
+int main(void);
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
