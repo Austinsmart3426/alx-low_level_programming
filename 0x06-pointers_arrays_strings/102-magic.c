a1e10bb97fb93ddca704727eb56acfa706c976f3
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-22 14:13:21 +0000

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+ * Add one line to this code,
+ * so that the program prints a[2] = 98,
+ * followed by a new line.
+ * You are not allowed to use the
+ * variable a in your new line of code
+ * You are not allowed to modify the variable p
+ * You can only write one statement
+ 
+ * You are not allowed to code anything else than
+ * the line of expected line of code at the expected line
+ * Your code should be written at line 19, before the ;
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
