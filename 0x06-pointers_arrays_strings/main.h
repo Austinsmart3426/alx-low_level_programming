351f4bf904c85acebfd891065c3d7b5064c67e40
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-22 12:34:10 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,23 +1,16 @@
-#ifndef "MAIN_H_INCLUDED"
-#define "MAIN_H_INCLUDED"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main.h file for 0x06. C - More pointers, arrays and strings
- * Description: Contains all prototype functions used
- * Return: always 0
- */
-
-int main(void);
-int _putchar(char c)
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *leet(char *);
-char *cap_string(char *);
-char *rot13(char *);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
