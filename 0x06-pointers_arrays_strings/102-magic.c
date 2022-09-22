94a98274460cf017e45b31d6ff3df7c222890e13
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-22 14:24:09 +0000

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 /*
  * Add one line to this code,
- * so that the program prints a[2] = 98,
- * followed by a new line.
- * You are not allowed to use the
- * variable a in your new line of code
+ * so that the program prints a[2] = 98, followed by a new line.
+ * You are not allowed to use the variable a in your new line of code
  * You are not allowed to modify the variable p
  * You can only write one statement
- 
- * You are not allowed to code anything else than
- * the line of expected line of code at the expected line
  * Your code should be written at line 19, before the ;
  */
 
