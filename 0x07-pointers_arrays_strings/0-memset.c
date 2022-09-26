396a70179ddcc8e1b4fe2591a1266515be8f3b9c
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-09-26 22:43:16 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,9 +13,10 @@ char *_memset(char *s, char b, unsigned int n)
 	char *memory = s;
 	unsigned int value = b;
 
-	for (i = 0; i < n; i++)
-	{
-	memory[i] = value;
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	s[i] = b;
 	}
-	return (memory);
+	return (s);
 }
