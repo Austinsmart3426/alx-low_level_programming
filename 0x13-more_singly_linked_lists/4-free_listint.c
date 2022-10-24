ff19e240a21c38a24663fc29347366ad5c895999
Augustine chinedu Ikegbunam <Ikegbunamchi2016@gmail.com>
2022-10-24 23:36:02 +0000

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *   * free_listint - A function that frees a listint_t list.
+ * @head: The head of the linked list 
+ * Return: Nothing
+ */
+	void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+	temp = head;
+	head = head->next;
+	free(temp);
+	}
+	free(head);
+}
